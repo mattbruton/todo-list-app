586c7117a396ac64de9c1e37ba94f2d97ef19e70
MATT BRUTON <the.bruton@gmail.com>
2016-05-19 00:39:34 -0500

    gulpfile.js now pointing to the correct directory paths, updated .jshintrc

--- app/app.js
+++ app/app.js
@@ -4,9 +4,11 @@ var app = angular.module('TodoApp', []);
 
 app.controller("NavCtrl", function($scope) {
   $scope.navItems = [{name: "Logout"},{name: "All Items"}, {name: "New Items"}];
-})
+});
 
 app.controller("TodoCtrl", function($scope) {
   $scope.welcome = "hello";
 
 });
+
+console.log("hello");
