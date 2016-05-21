06c5fbbf1312dc4f4dce8d2a37f340944b3d107f
MATT BRUTON <the.bruton@gmail.com>
2016-05-21 10:11:46 -0500

    added basic view functionality

--- app/app.js
+++ app/app.js
@@ -8,6 +8,20 @@ app.controller("NavCtrl", function($scope) {
 
 app.controller("TodoCtrl", function($scope) {
   $scope.welcome = "hello";
+
+  $scope.showListView = true;
+
+  $scope.newItem = function() {
+    $scope.showListView = false;
+    console.log("you clicked newItem");
+  };
+  $scope.allItem = function() {
+    $scope.showListView = true;
+    console.log("you clicked allItem");
+  };
+
+
+
 });
 
 console.log("hello");
