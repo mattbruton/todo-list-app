de58094400eb27a91218a51177168d49097487b2
MATT BRUTON <the.bruton@gmail.com>
2016-05-23 19:36:14 -0500

    added working delete button

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,17 +1,32 @@
 "use strict";
 
-app.controller("ItemListCtrl", function($scope,$http) {
+app.controller("ItemListCtrl", function($scope,$http,$location) {
 
   $scope.items = [];
 
-  $http.get("https://nss-matt-todo-app.firebaseio.com/items.json")
-    .success(function(itemObject) {
-      var itemCollection = itemObject;
-      Object.keys(itemCollection).forEach(function(key) {
-        itemCollection[key].id = key;
-        $scope.items.push(itemCollection[key]);
+
+  var getItems = function() {
+    $http.get("https://nss-matt-todo-app.firebaseio.com/items.json")
+      .success(function(itemObject) {
+       var itemCollection = itemObject;
+       Object.keys(itemCollection).forEach(function(key) {
+          itemCollection[key].id = key;
+         $scope.items.push(itemCollection[key]);
+        })
+      });
+    }
+
+  getItems();
+
+  $scope.itemDelete = function(itemId) {
+    console.log("", itemId);
+    $http
+      .delete(`https://nss-matt-todo-app.firebaseio.com/items/${itemId}.json`)
+      .success(function(response) {
+        $scope.items = [];
+        getItems();
       })
-    });
+  }
     
 
 });
