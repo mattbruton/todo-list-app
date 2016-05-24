e5e8622b9225ab03afb544b8ca561cd490c94f5a
MATT BRUTON <the.bruton@gmail.com>
2016-05-23 20:52:43 -0500

    added individual item view

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -1,5 +1,21 @@
 "use strict";
 
-app.controller("ItemViewCtrl", function($scope) { 
+app.controller("ItemViewCtrl", function($scope, $http, $routeParams) { 
+  $scope.items = [];
+  $scope.selectedItem = {};
+  console.log("", $routeParams.itemId);
+
+  $http.get(`https://nss-matt-todo-app.firebaseio.com/items.json`)
+      .success(function(itemObject) {
+       var itemCollection = itemObject;
+       Object.keys(itemCollection).forEach(function(key) {
+          itemCollection[key].id = key;
+          $scope.items.push(itemCollection[key]);
+
+          $scope.selectedItem = $scope.items.filter(function(item) {
+            return item.id === $routeParams.itemId;
+          })[0]
+        })
+      });
 
 });
